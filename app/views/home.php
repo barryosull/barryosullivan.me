2cec5c83115bb93244cd21e9c9deb22452ccf3e3
Barry O Sullivan <bosulli@tcd.ie>
2014-02-16 23:59:40 +0000

    Fixing type on homepage

--- app/views/home.php
+++ app/views/home.php
@@ -5,7 +5,7 @@
 	<div class="col-md-offset-2 col-md-8">
 
 		<img class="pull-right img-circle profile-pic" src="http://m.c.lnkd.licdn.com/mpr/mpr/shrink_80_80/p/5/005/021/3ca/06e0e0a.jpg">
-			I am a full-stack software developer living and working in Dublin.<br>I like building software that gets used, software that brings value to people's lives, be that in business or in their personal lives. I have strong interests in business, management, culture as well as all aspects of software development<br><br>Above all else, I enjoy learning. Why not click below to see what's I've learned recently?
+			I am a full-stack software developer living and working in Dublin.<br>I like building software that gets used, software that brings value to people's lives, be that in business or in their personal lives. I have strong interests in business, management, culture as well as all aspects of software development<br><br>Above all else, I enjoy learning. Why not click below to see what I've learned recently.
 		<hr>
 	</div>
 	<div class="col-md-12 what-I-am">
